7c01b6278bb5f54baaf34b414e1fde9a1b464677
Leon <hualiang.tian@gmail.com>
2021-08-30 19:00:13 -0400

    commit tree 23bcba0d5ebaae642a7412559e9cd29534971756

--- index.js
+++ index.js
@@ -10,4 +10,37 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+const morning = 12;
+const afternoon = 17;
+
+const g1 = "Good Morning";
+const g2 = "Good Afternoon";
+const g3 = "Good Evening";
+
+function greet(user_time) 
+{
+  let hh = user_time.split(':')
+  let hour = parseInt(hh[0])
+
+  if (hour < morning) 
+  {
+    return g1;
+  }
+
+  else if (hour >= morning && hour <= afternoon) 
+  {
+    return g2;
+  }
+
+  else 
+  {
+    return g3;
+  }
+  
+}
+
 /* Write your implementation of displayMessage() */
+function displayMessage(msg)
+{
+  document.getElementById("greeting").innerText = msg;
+}
